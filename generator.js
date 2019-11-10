9547b70f13df92d87319d69ebda389e02936cbe1
Minh Ha <vanminh0910@gmail.com>
2019-11-10 08:07:11 +0700

    T

--- generator.js
+++ generator.js
@@ -3,9 +3,3 @@ Blockly.Python['yolobit_extension3'] = function(block) {
   var code = 'print("Hello Yolo:Bit 3")';
   return code;
 };
-
-Blockly.Python['yolobit_extension4'] = function(block) {
-  // TODO: Assemble Python into code variable.
-  var code = 'print("Hello Yolo:Bit 4")';
-  return code;
-};
